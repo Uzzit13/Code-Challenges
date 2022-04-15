3df53524ea9130ca18f7680cc90f65dd842eeb9d
Tony <munteanuantonio95@gmail.com>
2022-04-15 12:23:39 +0100

    8 KYU kata

---
+++ 15-04-2022/app.js
@@ -0,0 +1,11 @@
+/*
+
+https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/javascript
+
+Write a function that checks if a given string (case insensitive) is a palindrome.
+
+*/
+
+function isPalindrome(x) {
+    return x.toLowerCase() === x.toLowerCase().split('').reverse().join('') ? true : false;
+};
