81471a9afb849f53e769768a6994a6346e15e6fa
Tony <munteanuantonio95@gmail.com>
2022-04-21 16:34:58 +0100

    8 KYU kata

---
+++ 21-04-2022/app.js
@@ -0,0 +1,33 @@
+/*
+
+https://www.codewars.com/kata/57f222ce69e09c3630000212/train/javascript
+
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+*/
+
+function well(x){
+    let goodness = 0
+    for (let i = 0; i <= x.length; i++){
+        if (x[i] === 'good'){
+        goodness += 1
+    }
+}
+    if (goodness > 2){
+        return 'I smell a series!'
+    }else if (goodness <= 2 && goodness > 0){
+        return 'Publish!'
+    }else{
+        return 'Fail!'
+    }
+}
+
+
+// SHORTER VERSION
+
+const well = x => {
+    const good_count = x.filter(x => x == 'good').length
+    return good_count < 1 ? 'Fail!' : good_count < 3 ? 'Publish!' : 'I smell a series!'
+}
