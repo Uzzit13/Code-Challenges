2a19a62503c3308dec0d2ab52fe7332aef58fce6
Tony <munteanuantonio95@gmail.com>
2022-06-06 17:14:35 +0100

    8 KYU kata

---
+++ 06-06-2022/app.js
@@ -0,0 +1,19 @@
+/*
+
+8 KYU kata
+
+https://www.codewars.com/kata/56b1f01c247c01db92000076/javascript
+
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+Good Luck!
+
+*/
+
+function doubleChar(str) {
+    return str.split('').map(e => e + e).join('');
+}
