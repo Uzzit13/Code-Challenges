b18aa89e606bceb80c1e4e6b3f899483697c4a8d
Tony <munteanuantonio95@gmail.com>
2022-06-08 17:48:43 +0100

    8 KYU kata

---
+++ 08-06-2022/app.js
@@ -0,0 +1,18 @@
+/*
+
+8 KYU kata
+
+https://www.codewars.com/kata/5625618b1fe21ab49f00001f/solutions/javascript
+
+Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+
+*/
+
+function sayHello(name) {
+    return `Hello, ${name}`
+}
