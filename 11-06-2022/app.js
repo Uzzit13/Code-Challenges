0fe20f61635d3bbb9e3f47699e6cf62e738b43c6
Tony <munteanuantonio95@gmail.com>
2022-06-11 08:58:09 +0100

    7 KYU kata

---
+++ 11-06-2022/app.js
@@ -0,0 +1,15 @@
+/*
+
+7 KYU kata
+
+https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+*/
+
+function findShorts(s){
+    return s.split(' ').sort((a,b) => a.length - b.length).shift()
+}
