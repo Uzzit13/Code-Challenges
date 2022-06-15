b7c2e26a83e7b35289863d1bafd14a51b75e6833
Tony <munteanuantonio95@gmail.com>
2022-06-15 19:59:46 +0100

    8 KYU kata

---
+++ 15-06-2022/app.js
@@ -0,0 +1,13 @@
+/*
+
+8 KYU kata
+
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+
+Write a function which converts the input string to uppercase.
+
+*/
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
