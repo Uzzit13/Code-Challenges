2313ea92056c7b5859c3dba2c9f1ae1921ab586f
Tony <munteanuantonio95@gmail.com>
2022-07-09 18:56:03 +0100

    8 KYU kata

---
+++ 09-07-2022/app.js
@@ -0,0 +1,11 @@
+/*
+
+8 KYU kata
+
+Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+
+*/
+
+function logs(x, a, b) {
+    return (Math.log(a) / Math.log(x)) + (Math.log(b) / Math.log(x))
+}
