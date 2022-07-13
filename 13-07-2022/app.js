79e1c932085944c9e99025d896a9d7d8e39a5c93
Tony <munteanuantonio95@gmail.com>
2022-07-13 22:23:53 +0100

    8 KYU kata

---
+++ 13-07-2022/app.js
@@ -0,0 +1,11 @@
+/*
+
+8 KYU kata
+
+Complete the function which converts hex number (given as a string) to a decimal number.
+
+*/
+
+function hexToDec(hexString) {
+    return parseInt(hexString, 16);
+}
