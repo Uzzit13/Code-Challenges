0dd23a95e9bedb85f9bddf2f28635d91ef9eb26f
Tony <munteanuantonio95@gmail.com>
2022-07-22 20:22:50 +0100

    8 KYU kata

---
+++ 22-07-2022/app.js
@@ -0,0 +1,11 @@
+/*
+
+8 KYU kata
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+*/
+
+function simpleMultiplication(number) {
+    return number % 2 == 0 ? number * 8 : number * 9
+}
